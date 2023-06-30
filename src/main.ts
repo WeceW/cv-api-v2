5217a855319ecb0140a9b1c6e3a28b9b3ca6b51f
WeceW <toni.weckroth@gmail.com>
2023-06-30 20:06:31 +0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://toni-weckroth.onrender.com/'],
-  });
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
 
