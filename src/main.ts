bd5423aa7c892608981724631eb504292a488399
WeceW <toni.weckroth@gmail.com>
2023-06-30 12:53:21 +0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://toni-weckroth.onrender.com/'],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(1337);
 }
+
 bootstrap();
