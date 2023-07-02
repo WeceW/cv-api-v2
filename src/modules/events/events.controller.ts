a3e538f0d4963a1ddc4e0fb770a7e1822fa7cc21
WeceW <toni.weckroth@gmail.com>
2023-07-02 20:48:50 +0300

    Get all events & get analytics data

--- src/modules/events/events.controller.ts
+++ src/modules/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { Public } from 'src/decorators/Public';
 import { CreateEventDto } from './event.entity';
 import { EventsService } from './events.service';
@@ -11,5 +11,15 @@ export class EventsController {
   @Post()
   create(@Body() event: CreateEventDto) {
     return this.eventsService.create(event);
+  }
+
+  @Get()
+  findAll(@Query() query: { filter?: string }) {
+    return this.eventsService.findAll(query.filter);
+  }
+
+  @Get('analytics')
+  analytics() {
+    return this.eventsService.analytics();
   }
 }
--- src/modules/events/events.service.ts
+++ src/modules/events/events.service.ts
@@ -16,4 +16,113 @@ export class EventsService {
   async create(payload: CreateEventDto) {
     return await this.eventsRepository.save(payload);
   }
+
+  /**
+   * Find all events
+   **/
+  async findAll(filter?: string): Promise<Event[]> {
+    return await this.eventsRepository.find({
+      where: { event: filter },
+      order: { timestamp: 'DESC' },
+    });
+  }
+
+  /**
+   * Get analytics
+   */
+  async analytics() {
+    return {
+      totalEvents: await this.eventsRepository.count(),
+
+      uniqueSessions: (
+        await this.eventsRepository
+          .createQueryBuilder('event')
+          .select('COUNT(DISTINCT "sessionID")', 'count')
+          .getRawOne()
+      )?.count,
+
+      averageEventsPerSession: (
+        await this.eventsRepository
+          .createQueryBuilder('event')
+          .select('COUNT("sessionID") / COUNT(DISTINCT "sessionID")', 'count')
+          .getRawOne()
+      )?.count,
+
+      eventsPerDay: await this.eventsRepository
+        .createQueryBuilder('event')
+        .select('DATE("timestamp")', 'date')
+        .addSelect('COUNT(*)', 'count')
+        .groupBy('date')
+        .orderBy('date', 'DESC')
+        .getRawMany(),
+
+      pageViews: await this.eventsRepository
+        .createQueryBuilder('event')
+        .select('"path"', 'path')
+        .addSelect('COUNT(*)', 'count')
+        .where('"path" IS NOT NULL')
+        .groupBy('path')
+        .orderBy('count', 'DESC')
+        .getRawMany(),
+
+      linksClicked: await this.eventsRepository
+        .createQueryBuilder('event')
+        .select('"linkTarget"', 'linkTarget')
+        .addSelect('COUNT(*)', 'count')
+        .where('"linkTarget" IS NOT NULL')
+        .groupBy('"linkTarget"')
+        .orderBy('count', 'DESC')
+        .getRawMany(),
+
+      eventsCount: await this.eventsRepository
+        .createQueryBuilder('event')
+        .select('"event"', 'event')
+        .addSelect('COUNT(*)', 'count')
+        .groupBy('event')
+        .orderBy('count', 'DESC')
+        .getRawMany(),
+
+      employers: (
+        await this.eventsRepository
+          .createQueryBuilder('event')
+          .select('"employer"')
+          .where('"employer" IS NOT NULL')
+          .groupBy('"employer"')
+          .getRawMany()
+      ).map((e) => e.employer),
+
+      eventsPerColorTheme: await this.eventsRepository
+        .createQueryBuilder('event')
+        .select('"colorTheme"', 'colorTheme')
+        .addSelect('COUNT(*)', 'count')
+        .groupBy('"colorTheme"')
+        .orderBy('count', 'DESC')
+        .getRawMany(),
+
+      uniqueSessionsPerScreenWidth: await this.eventsRepository
+        .createQueryBuilder('event')
+        .select(
+          'FLOOR("screenWidth"::float / 160) * 160 || \' - \' || (FLOOR("screenWidth"::float / 160) + 1) * 160',
+          'range',
+        )
+        .addSelect('COUNT(DISTINCT "sessionID")', 'count')
+        .where('"screenWidth" IS NOT NULL')
+        .groupBy('range')
+        .orderBy('range', 'DESC')
+        .getRawMany(),
+
+      uniqueSessionsPerScreenOrientation: await this.eventsRepository
+        .createQueryBuilder('event')
+        .select(
+          'CASE WHEN "screenWidth" > "screenHeight" THEN \'Landscape\' ELSE \'Portrait\' END',
+          'orientation',
+        )
+        .addSelect('COUNT(DISTINCT "sessionID")', 'count')
+        .where('"screenWidth" IS NOT NULL')
+        .andWhere('"screenHeight" IS NOT NULL')
+        .groupBy('orientation')
+        .orderBy('orientation', 'DESC')
+        .getRawMany(),
+    };
+  }
 }
